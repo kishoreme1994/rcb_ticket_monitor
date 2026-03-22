bed3100228251dffe7529426898e448d3035984e
kishoreme1994 <kishore.me.1994@gmail.com>
2026-03-22 09:51:59 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,7 +4,7 @@ import time
 BOT_TOKEN = "8577847140:AAGufo6iYE4jMacG0O7LOML7bbPOQlUcu_c"
 CHAT_ID = "851468594"
 
-URL = "https://shop.royalchallengers.com/ticket"  # change if needed
+URL = "https://shop.royalchallengers.com"  # change if needed
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
